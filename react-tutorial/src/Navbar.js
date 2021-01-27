e84d0b5f485946b30904a2d175c37eaf61ef341a
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-27 22:41:40 +0530

    Update Navbar.js

--- react-tutorial/src/Navbar.js
+++ react-tutorial/src/Navbar.js
@@ -1,13 +1,16 @@
+import Link from "react-router-dom/modules/Link";
+
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
+                <Link href="/">Home</Link>
+                <Link href="/create" style={{
                     color:"pink",
                     backgroundColor:"green",
-                }}>New Blog</a>
+                }}>New Blog</Link>
             </div>
         </nav>
     );
