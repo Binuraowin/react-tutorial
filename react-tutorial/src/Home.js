499e5f5fdc2d1bcef584d4e738f34c2aa5708ede
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-27 10:27:47 +0530

    api

--- react-tutorial/src/Home.js
+++ react-tutorial/src/Home.js
@@ -5,13 +5,15 @@ const Home = () => {
     const [fliter,setFilter]= useState('binura')
     const [name,setName]= useState('binura')
 
-    const [blogs, setBlogs] = useState([
-            { title: 'My new website', body: 'lorem ipsum...', author: 'my', id: 1 },
-            { title: 'Welcome party!', body: 'lorem ipsum...', author: 'my', id: 2 },
-            { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'binura', id: 3 },
-            { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'binura', id: 4 },
-            { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'binura', id: 5 }
-            ])
+    // const [blogs, setBlogs] = useState([
+    //         { title: 'My new website', body: 'lorem ipsum...', author: 'my', id: 1 },
+    //         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'my', id: 2 },
+    //         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'binura', id: 3 },
+    //         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'binura', id: 4 },
+    //         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'binura', id: 5 }
+    //         ])
+
+    const [blogs, setBlogs] = useState(null);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -22,16 +24,24 @@ const Home = () => {
         setBlogs(newBlogs);
     }
     useEffect(()=>{
+        fetch('http://localhost:8000/blogs')
+            .then(res =>{
+                return res.json();
+            })
+            .then(data =>{
+                setBlogs(data)
+            })
         console.log('use effect');
-        console.log(name);
-    },[name]);
+
+    },[]);
 
     return (
             <div className="home">
                 <button onClick={filter}>filter</button>
                 <button onClick={()=>setName('buruwa')}>filter</button>
-             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-             <BlogList blogs={blogs.filter((blog)=>blog.author==='my')} title="My Blogs" handleDelete={handleDelete}/>
+                {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
+                { blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'my')} title="My Blogs"
+                           handleDelete={handleDelete}/>}
             </div>
         );
 }
